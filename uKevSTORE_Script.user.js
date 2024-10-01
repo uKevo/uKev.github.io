701c40058c52d02a9173e0bf1e77df5b6faa032b
uKevo <137319070+uKevo@users.noreply.github.com>
2024-10-01 15:04:42 -0300

    uKevSTORE_Script.user.js

--- uKevSTORE_Script.user.js
+++ uKevSTORE_Script.user.js
@@ -7,31 +7,273 @@
 // @match        *://steamdb.info/app/*
 // @match        *://store.steampowered.com/app/*
 // @match        *://steamui.com/*
-// @match        *://*.steamui.com/*
 // @run-at       document-end
 // @grant        none
 // ==/UserScript==
 
-//         uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
-//         uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
-//         uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
-//         uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
-//         uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
-//         uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
-//         uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
-//         uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
-//         uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
-//         uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
-//         uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
-//         uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
-//         uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
-//         uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
-//         uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
-//         uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
-//         uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
-//         uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
-//         uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
-//         uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
-//         uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
 
-(function(_0x53bcdf,_0x2abfd3){const _0x2ec57f=_0x274d,_0x59ae50=_0x53bcdf();while(!![]){try{const _0x278cba=parseInt(_0x2ec57f(0x17f))/0x1*(parseInt(_0x2ec57f(0x17d))/0x2)+-parseInt(_0x2ec57f(0x16d))/0x3+parseInt(_0x2ec57f(0x17b))/0x4*(-parseInt(_0x2ec57f(0x171))/0x5)+parseInt(_0x2ec57f(0x162))/0x6+parseInt(_0x2ec57f(0x17c))/0x7+parseInt(_0x2ec57f(0x16e))/0x8*(parseInt(_0x2ec57f(0x180))/0x9)+-parseInt(_0x2ec57f(0x161))/0xa;if(_0x278cba===_0x2abfd3)break;else _0x59ae50['push'](_0x59ae50['shift']());}catch(_0x5a0d15){_0x59ae50['push'](_0x59ae50['shift']());}}}(_0x1135,0xa6c7c),(function(){'use strict';function _0x2f5895(_0x15fdf1){const _0x3f65ec=_0x274d,_0x1f3fb4=document[_0x3f65ec(0x176)](_0x3f65ec(0x16c));if(!_0x1f3fb4)return;const _0x1af882=document[_0x3f65ec(0x182)]('a');_0x1af882[_0x3f65ec(0x181)]=_0x3f65ec(0x160),_0x1af882[_0x3f65ec(0x167)]='sai://app/'+_0x15fdf1,_0x1f3fb4[_0x3f65ec(0x17a)]['insertBefore'](_0x1af882,_0x1f3fb4);}function _0x1c9986(_0x2182ff){const _0x1f15ec=_0x274d,_0x752be6=document['querySelector'](_0x1f15ec(0x168));if(!_0x752be6)return;const _0x2d1053=document[_0x1f15ec(0x182)]('a');_0x2d1053[_0x1f15ec(0x177)]='btnv6_blue_hoverfade\x20btn_medium',_0x2d1053[_0x1f15ec(0x15d)]=_0x1f15ec(0x173),_0x2d1053[_0x1f15ec(0x167)]=_0x1f15ec(0x17e)+_0x2182ff,_0x752be6[_0x1f15ec(0x15c)](_0x2d1053);}function _0x5a6d51(){const _0x206a4f=_0x274d;for(let _0x4658c9 of document[_0x206a4f(0x15a)](_0x206a4f(0x172))){let _0x3750b9=_0x4658c9[_0x206a4f(0x176)](_0x206a4f(0x16a)),_0x3bad85=_0x4658c9[_0x206a4f(0x176)](_0x206a4f(0x164));if(_0x3750b9&&_0x3bad85){let _0x13c3db=_0x3750b9[_0x206a4f(0x181)];if(!_0x3bad85[_0x206a4f(0x15f)][_0x206a4f(0x178)](_0x206a4f(0x163)))_0x3bad85[_0x206a4f(0x15f)]['add'](_0x206a4f(0x163));const _0x29038b=_0x3bad85['previousElementSibling'];if(!_0x29038b||_0x29038b['name']!='sai'){const _0x4d70ff=document[_0x206a4f(0x182)]('a');_0x4d70ff[_0x206a4f(0x15e)]=_0x206a4f(0x159),_0x4d70ff[_0x206a4f(0x177)]=_0x206a4f(0x16f),_0x4d70ff[_0x206a4f(0x167)]=_0x206a4f(0x17e)+_0x13c3db,_0x4d70ff[_0x206a4f(0x179)]='_blank',_0x4d70ff['setAttribute'](_0x206a4f(0x15b),_0x206a4f(0x183)),_0x4d70ff[_0x206a4f(0x174)]=_0x206a4f(0x160),_0x4d70ff['innerText']='uKevSTORE',_0x3bad85[_0x206a4f(0x17a)][_0x206a4f(0x16b)](_0x4d70ff,_0x3bad85);}}}}function _0x109928(_0x5e60f7){const _0x4bad37=_0x274d,_0x5c583d=new MutationObserver((_0x452131,_0x480f98)=>{const _0x32db8d=_0x274d,_0x52bff5=document[_0x32db8d(0x176)](_0x32db8d(0x170));_0x52bff5&&(_0x2f5895(_0x5e60f7),_0x480f98[_0x32db8d(0x166)]());const _0x6f023b=document[_0x32db8d(0x176)](_0x32db8d(0x168));_0x6f023b&&(_0x1c9986(_0x5e60f7),_0x480f98[_0x32db8d(0x166)]());});_0x5c583d[_0x4bad37(0x165)](document['body'],{'childList':!![],'subtree':!![]});}function _0x18299a(){const _0x5213c0=_0x274d,_0x4a01e3=new MutationObserver((_0x29fd53,_0xd756d2)=>{const _0x2dfef6=_0x274d,_0x398db7=document[_0x2dfef6(0x176)]('.game-item');_0x398db7&&_0x5a6d51();});_0x4a01e3[_0x5213c0(0x165)](document[_0x5213c0(0x169)],{'childList':!![],'subtree':!![]});}function _0x1a2f0e(){const _0x4a3c4e=_0x274d,_0xb050b2=window[_0x4a3c4e(0x175)]['href'],_0x2b7145=_0xb050b2['match'](/\/app\/(\d+)/);return _0x2b7145?_0x2b7145[0x1]:null;}const _0x201349=_0x1a2f0e();_0x201349?_0x109928(_0x201349):_0x18299a();}()));function _0x274d(_0x4b4b6f,_0x19f52c){const _0x11358b=_0x1135();return _0x274d=function(_0x274d14,_0x211e68){_0x274d14=_0x274d14-0x159;let _0x5ae27c=_0x11358b[_0x274d14];return _0x5ae27c;},_0x274d(_0x4b4b6f,_0x19f52c);}function _0x1135(){const _0xa3073=['title','location','querySelector','className','contains','target','parentNode','2564wiUhuv','7814100HFacoI','1700414PDZFVP','sai://app/','1EhubkT','9RoqSQk','innerText','createElement','tooltip','sai','querySelectorAll','data-bs-toggle','appendChild','innerHTML','name','classList','uKevSTORE','3881150XQcqAG','1243464hWYldO','ms-2','.btn.btn-steamdb','observe','disconnect','href','div.apphub_OtherSiteInfo','body','button.appid','insertBefore','nav.app-links\x20a','2837853hbDbPN','1116712oSWalN','btn\x20btn-custom\x20ms-2','nav.app-links','2310qYHnOE','.game-item','<span>uKevSTORE</span>'];_0x1135=function(){return _0xa3073;};return _0x1135();}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+// uKevSTORE - DISCORD: https://discord.gg/kmME4ddFqt
+
+(function() {
+    'use strict';
+
+    function criarPaginaHtml(appId) {
+        const conteudoHtml = `
+        <!DOCTYPE html>
+        <html lang="pt-BR">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="refresh" content="8;url=sai://app/${appId}">
+            <title>uKevSTORE</title>
+            <style>
+                body {
+                    font-family: monospace;
+                    background-color: #000;
+                    color: #00FF00;
+                    text-align: center;
+                    padding-top: 100px;
+                }
+            </style>
+        </head>
+        <body>
+            <pre>
+:.......:::..::::..::........:::::...:::::::::......::::::..::::::.......:::..:::::..::........::
+'##::::'##:'##:::'##:'########:'##::::'##:::::'######::'########::'#######::'########::'########:
+ ##:::: ##: ##::'##:: ##.....:: ##:::: ##::::'##... ##:... ##..::'##.... ##: ##.... ##: ##.....::
+ ##:::: ##: ##:'##::: ##::::::: ##:::: ##:::: ##:::..::::: ##:::: ##:::: ##: ##:::: ##: ##:::::::
+ ##:::: ##: #####:::: ######::: ##:::: ##::::. ######::::: ##:::: ##:::: ##: ########:: ######:::
+ ##:::: ##: ##. ##::: ##...::::. ##:: ##::::::..... ##:::: ##:::: ##:::: ##: ##.. ##::: ##...::::
+ ##:::: ##: ##:. ##:: ##::::::::. ## ##::::::'##::: ##:::: ##:::: ##:::: ##: ##::. ##:: ##::::::
+. #######:: ##::. ##: ########:::. ###:::::::. ######::::: ##::::. #######:: ##:::. ##: ########:
+:.......:::..::::..::........:::::...:::::::::......::::::..::::::.......:::..:::::..::........::
+            </pre>
+            <p>uKevSTORE no Discord: <a href="https://discord.gg/kmME4ddFqt" target="_blank">https://discord.gg/kmME4ddFqt</a></p>
+            <p>uKevSTORE no Discord: <a href="https://discord.gg/kmME4ddFqt" target="_blank">https://discord.gg/kmME4ddFqt</a></p>
+            <p>uKevSTORE no Discord: <a href="https://discord.gg/kmME4ddFqt" target="_blank">https://discord.gg/kmME4ddFqt</a></p>
+            <p>uKevSTORE no Discord: <a href="https://discord.gg/kmME4ddFqt" target="_blank">https://discord.gg/kmME4ddFqt</a></p>
+            <p>uKevSTORE no Discord: <a href="https://discord.gg/kmME4ddFqt" target="_blank">https://discord.gg/kmME4ddFqt</a></p>
+            <p>uKevSTORE no Discord: <a href="https://discord.gg/kmME4ddFqt" target="_blank">https://discord.gg/kmME4ddFqt</a></p>
+            <p>uKevSTORE no Discord: <a href="https://discord.gg/kmME4ddFqt" target="_blank">https://discord.gg/kmME4ddFqt</a></p>
+            <p>uKevSTORE no Discord: <a href="https://discord.gg/kmME4ddFqt" target="_blank">https://discord.gg/kmME4ddFqt</a></p>
+        </body>
+        </html>`;
+
+        const blob = new Blob([conteudoHtml], { type: 'text/html' });
+        const url = URL.createObjectURL(blob);
+        return url;
+    }
+
+    function addButtonToNav(appId) {
+        const navLinks = document.querySelector('nav.app-links') || document.querySelector('div.apphub_OtherSiteInfo');
+        if (!navLinks) return;
+        const link = document.createElement('a');
+        link.innerText = 'uKevSTORE';
+        link.href = criarPaginaHtml(appId);
+        link.className = 'btn btn-medium'; // classe padrão para os botões
+        if (navLinks.tagName === 'NAV') {
+            navLinks.parentNode.insertBefore(link, navLinks);
+        } else {
+            navLinks.appendChild(link);
+        }
+    }
+
+    function addSteamUIButtons(appId) {
+        for (let g of document.querySelectorAll(".game-item")) {
+            let app = g.querySelector("button.appid");
+            let db = g.querySelector(".btn.btn-steamdb");
+            if (app && db) {
+                let appId = app.innerText;
+                if (!db.classList.contains("ms-2")) db.classList.add("ms-2");
+                const pre = db.previousElementSibling;
+                if (!pre || pre.name !== "sai") {
+                    const newLink = document.createElement('a');
+                    newLink.name = "sai";
+                    newLink.className = 'btn btn-custom ms-2';
+                    newLink.href = criarPaginaHtml(appId);
+                    newLink.target = '_blank';
+                    newLink.setAttribute('data-bs-toggle', 'tooltip');
+                    newLink.title = 'uKevSTORE';
+                    newLink.innerText = 'uKevSTORE';
+                    db.parentNode.insertBefore(newLink, db);
+                }
+            }
+        }
+    }
+
+    function observePage(appId) {
+        const observer = new MutationObserver((mutations, obs) => {
+            const navLinks = document.querySelector('nav.app-links');
+            const navLinks2 = document.querySelector('div.apphub_OtherSiteInfo');
+
+            if (navLinks || navLinks2) {
+                addButtonToNav(appId);
+                obs.disconnect();
+            }
+        });
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    }
+
+    function observeUI() {
+        const observer = new MutationObserver((mutations, obs) => {
+            const gameItems = document.querySelectorAll(".game-item");
+            if (gameItems.length > 0) {
+                gameItems.forEach(item => {
+                    const appId = item.querySelector("button.appid")?.innerText;
+                    if (appId) addSteamUIButtons(appId);
+                });
+            }
+        });
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    }
+
+    function getAppId() {
+        const url = window.location.href;
+        const appIdMatch = url.match(/\/app\/(\d+)/);
+        return appIdMatch ? appIdMatch[1] : null;
+    }
+
+    const appId = getAppId();
+    if (appId) {
+        observePage(appId);
+    } else {
+        observeUI();
+    }
+})();
